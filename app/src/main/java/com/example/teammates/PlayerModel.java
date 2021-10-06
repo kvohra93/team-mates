03b03be0651344470152fb145417ebb277a95036
Kunal <hackedin2@gmail.xcm>
2021-10-06 10:55:30 -0500

    Initial commit -added player model and class

---
+++ app/src/main/java/com/example/teammates/PlayerModel.java
@@ -0,0 +1,33 @@
+package com.example.teammates;
+
+import java.util.ArrayList;
+
+public class PlayerModel {
+
+ArrayList available;
+ArrayList teams;
+
+public void addPlayerToTeam(int position, int team){
+
+
+}
+
+public void reset(){}
+
+    public class Player{
+        private String Name;
+
+        public Player(String name) {
+            Name = name;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        @Override
+        public String toString() {
+            return Name ;
+        }
+    }
+}
